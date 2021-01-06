b53a64e496f0f0ed4360d9d2532e9d4a72a86cbe
redou <redouanemessaoudi87@gmail.com>
2021-01-06 14:40:05 -0500

    broken links exercise

--- src/test/java/BrokenLinks.java
+++ src/test/java/BrokenLinks.java
@@ -1,4 +1,11 @@
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
     public class BrokenLinks {
@@ -45,7 +52,7 @@ import java.util.concurrent.TimeUnit;
 
 
             driver.quit();
-            System.out.println("hello");
+
 
         }
 
